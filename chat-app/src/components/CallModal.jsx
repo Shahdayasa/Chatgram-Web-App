459a2ee070169e634082a7cb7a07b1335f023853
shahd <shahdayasa46@gmail.com>
2026-08-24 10:42:31 +0300

    add call State to Call Modal function

--- chat-app/src/components/CallModal.jsx
+++ chat-app/src/components/CallModal.jsx
@@ -1,21 +1,39 @@
 export default function CallModal({
   selectedUser,
+  callState,
   onEndCall,
 }) {
+
+    const isConnected = callState === "connected";
   return (
     <div className="call-modal-overlay">
       <div className="call-modal">
-        <h2>Calling...</h2>
+        <h2>{isConnected ? "Call Connected" : "Calling..."}</h2>
 
         <div className="call-avatar">
-          {selectedUser?.name?.charAt(0)?.toUpperCase()}
+          {selectedUser?.avatar? (
+            <img 
+            src = {selectedUser?.avatar}
+            alt= {selectedUser?.name || "User"}
+            className="call-avatar-image"
+            />
+          ) : (
+         selectedUser?.name?.charAt(0)?.toUpperCase() || "?"
+          )}
         </div>
 
-        <h3>{selectedUser?.name}</h3>
+        <h3>{selectedUser?.name || "Unkown User"}</h3>
 
-        <p>Connecting...</p>
+        <p>
+            {
+                isConnected
+                ? "connected"
+                : "Waiting for answer..."
+            }
+        </p>
 
         <button
+          type="button"
           className="end-call-button"
           onClick={onEndCall}
         >
